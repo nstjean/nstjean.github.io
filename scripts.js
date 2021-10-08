b78cf8826fc5b26b09379313a256b27bfce04f48
Natalie St Jean <natalie.stjean@gmail.com>
2021-10-08 13:07:14 -0400

    add resume content

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	// NAV BAR TOGGLE
+	// opens and closes nav drop down from toggle button
+	//
+	document.getElementById('toggle').addEventListener('click', function() {
+		document.getElementById('menu-left').classList.toggle('active');
+		document.getElementById('menu-right').classList.toggle('active');
+		document.getElementById('nav-bar').classList.toggle('active');
+	});
+
+
+})
